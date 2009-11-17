c375a4dd36dbc63b95029031b331dea9b2728495
crazyjin7 <crazyjin7@ddb2e5e6-d286-11de-a69b-f5a595c35080>
2009-11-17 07:30:27 +0000

    Spring AOP pointcut 적용 후

--- FrameworkWeb/src/ch03/aop/pointcut/GargleAdvice.java
+++ FrameworkWeb/src/ch03/aop/pointcut/GargleAdvice.java
@@ -1,6 +1,16 @@
 package ch03.aop.pointcut;
 
-public class GargleAdvice {
+import java.lang.reflect.Method;
 
-	
+import org.springframework.aop.AfterReturningAdvice;
+
+public class GargleAdvice implements AfterReturningAdvice {
+
+	public void afterReturning(Object arg0, Method arg1, Object[] arg2, Object arg3) throws Throwable {
+		System.out.println("    ---> 양치한다.");
+
+	}
+
+
+
 }
