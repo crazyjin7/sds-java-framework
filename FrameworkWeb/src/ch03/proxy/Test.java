2b2e952ae1ac627170d518d32e0ab30390e517ea
crazyjin7 <crazyjin7@ddb2e5e6-d286-11de-a69b-f5a595c35080>
2009-11-17 04:23:56 +0000

    proxy (2)

--- FrameworkWeb/src/ch03/proxy/Test.java
+++ FrameworkWeb/src/ch03/proxy/Test.java
@@ -1,9 +1,18 @@
 package ch03.proxy;
 
+import java.lang.reflect.Proxy;
+
 public class Test {
 
 	public static void main(String[] args) throws Exception {
-		EmpManager empManager = new EmpManagerImpl();
+		//EmpManager empManager = new EmpManagerImpl();
+
+		LoggingHandler handler = new LoggingHandler(new EmpManagerImpl());
+		// newProxyInstance(대상 객체의 클래스 로더, 구현해야 할 인터페이스 목록, InvocationHandler 객체)
+		EmpManager empManager = (EmpManager) Proxy.newProxyInstance(
+				EmpManagerImpl.class.getClassLoader(),
+				EmpManagerImpl.class.getInterfaces(),
+				handler);
 
 		System.out.println(empManager.getGreetingMessage("홍길동"));
 	}
