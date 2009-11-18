1b6f6797e5f1884ac9689c17639588687712fcba
crazyjin7 <crazyjin7@ddb2e5e6-d286-11de-a69b-f5a595c35080>
2009-11-18 02:34:14 +0000

    Spring JDBC (4) - spring jdbc

--- FrameworkWeb/src/ch04/springjdbc/MagazineDAO.java
+++ FrameworkWeb/src/ch04/springjdbc/MagazineDAO.java
@@ -1,18 +1,11 @@
 package ch04.springjdbc;
 
-import javax.sql.DataSource;
-
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
-public class MagazineDAO {
+public class MagazineDAO extends JdbcDaoSupport {
 
-	private DataSource dataSource;
 	private RowMapper magazineRowMapper;
-
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
 
 	public void setMagazineRowMapper(RowMapper magazineRowMapper) {
 		this.magazineRowMapper = magazineRowMapper;
@@ -20,8 +13,6 @@ public class MagazineDAO {
 
 	public Magazine findMagazine(int magazineId) {
 		String sql = "select magazineId, title, price, created from MAGAZINE where magazineId = ?";
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-		Object[] params = { magazineId };
-		return (Magazine) jdbcTemplate.queryForObject(sql, params, magazineRowMapper);
+		return (Magazine) getJdbcTemplate().queryForObject(sql, new Object[] { magazineId }, magazineRowMapper);
 	}
 }
