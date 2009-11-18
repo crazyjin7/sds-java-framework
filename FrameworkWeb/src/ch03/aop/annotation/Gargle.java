75d0fc1f75de3bed1939d78a6ae7e43f17f92e84
crazyjin7 <crazyjin7@ddb2e5e6-d286-11de-a69b-f5a595c35080>
2009-11-18 00:53:37 +0000

    AOP - AspectJ 스타일의 annotation (2)

--- FrameworkWeb/src/ch03/aop/annotation/Gargle.java
+++ FrameworkWeb/src/ch03/aop/annotation/Gargle.java
@@ -9,6 +9,8 @@ public class Gargle {
 	//사후 충고
 	//CityLife 빈의 eat로 시작하는 메소드
 	@After("execution(* CityLife.eat*())")
+	//@After("execution(String CityLife.eat*(String))")
+	//@After("execution(String CityLife.eat*(..))")
 	public void goGargle() throws Throwable {
 		System.out.println("---> 양치한다.");
 
